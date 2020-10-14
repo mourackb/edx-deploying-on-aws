631240b532295b9894c830e11e2b702f09806446
mourackb <moura.lucas@gmail.com>
2020-10-15 08:38:57 +1300

    Switching to us-east-1

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "dev" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/18"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "tf-example"
